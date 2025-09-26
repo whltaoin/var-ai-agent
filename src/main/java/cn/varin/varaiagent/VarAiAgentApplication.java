28638f18b34682c2b2736fcbeed30e1471ec8872
Varin <whltaoin@163.com>
2025-09-26 14:51:04 +0800

    init

---
+++ src/main/java/cn/varin/varaiagent/VarAiAgentApplication.java
@@ -0,0 +1,13 @@
+package cn.varin.varaiagent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VarAiAgentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VarAiAgentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/varin/varaiagent/VarAiAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.varin.varaiagent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VarAiAgentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
